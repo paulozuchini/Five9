b75c9376f116db81f144f2a6f9a17f9c95e3bfcc
Paulo Zuchini <paulo.hilton@gmail.com>
2024-09-03 18:06:14 +0100

    adding first test

--- Tests/Five9.Test/CallCenterEventServiceTest.cs
+++ Tests/Five9.Test/CallCenterEventServiceTest.cs
@@ -1,11 +1,42 @@
+using Five9.Library.DTO;
+using Five9.Services.Interfaces;
+using FluentAssertions;
+using Moq;
+
 namespace Five9.Test
 {
     public class CallCenterEventServiceTest
     {
         [Fact]
-        public void Test1()
+        public async void ServiceShouldGetAll()
         {
+            var mockService = new Mock<ICallCenterEventService>();
+            var mockResult = new List<CallCenterEventDto>()
+            {
+                new CallCenterEventDto()
+                {
+                    Id = 1,
+                    AgentId = new Guid("6B29FC40-CA47-1067-B31D-00DD010662DA"),
+                    AgentName = "John Smith",
+                    TimestampUtc = DateTime.UtcNow,
+                    Action = "CALL_STARTED",
+                    QueueIds = new List<Guid>()
+                    {
+                        new Guid("3a8cc33a-3f09-4ce5-9c53-e94585a410c8"),
+                        new Guid("3d887de3-8351-4391-b155-e174f472456a")
+                    }
+                }
+            };
 
+            mockService.Setup(s => s.GetAll()).Returns(mockResult);
+
+            var result = mockService.Object.GetAll();
+
+            result.Should().BeOfType<List<CallCenterEventDto>>()
+                .Which.FirstOrDefault()
+                .AgentId.Should().Be(new Guid("6B29FC40-CA47-1067-B31D-00DD010662DA"));
         }
+
+        //TODO ADD MORE TESTS LATER.
     }
 }
